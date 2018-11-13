0c797ae036f7d656fee64c096df1eed080aeecc9
rlawpdud301 <rlawpdud301@naver.com>
2018-11-13 17:40:46 +0900

    departmentdao test

---
+++ src/main/java/jdbc_pool_c3p0_study/Main.java
@@ -0,0 +1,12 @@
+package jdbc_pool_c3p0_study;
+
+import jdbc_pool_c3p0_study.jdbc.MyDataSource;
+
+public class Main {
+
+   public static void main(String[] args) {
+      MyDataSource mds = MyDataSource.getInstance();
+
+   }
+
+}
---
+++ src/main/java/jdbc_pool_c3p0_study/jdbc/ConnectionProvider.java
@@ -0,0 +1,10 @@
+package jdbc_pool_c3p0_study.jdbc;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class ConnectionProvider {
+   public static Connection getConnection() throws SQLException {
+      return MyDataSource.getInstance().getDataSource().getConnection();
+   }
+}
